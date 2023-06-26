450fa8f40f1cebfd67e8d4f54a2944d33f5d2c9d
unknown <>
2023-06-26 11:14:54 +0200

    OrderManager 2

--- Model/OrderManager.php
+++ Model/OrderManager.php
@@ -7,5 +7,24 @@ class OrderManager extends ModelManager{
     {
         parent::__construct("orders");
     }
+
+    public function getByUserId($id)
+    {
+        $req = $this->bdd->prepare("SELECT * FROM " . $this->table . " WHERE user_id = :id");
+        $req->bindParam(":id", $id);
+        $req->execute();
+        $req->setFetchMode(\PDO::FETCH_OBJ);
+        return $req->fetch();
+    }
+
+    public function GetOrderByStatus($userId, $status)
+    {
+        $req = $this->bdd->prepare("SELECT * FROM " . $this->table . " WHERE status = :status AND user_id = :userId ");
+        $req->bindParam(":userId", $userId);
+        $req->bindParam(":status", $status);
+        $req->execute();
+        $req->setFetchMode(\PDO::FETCH_OBJ);
+        return $req->fetch();
+    }
     
 }
