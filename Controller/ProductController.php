7db53faf6a3fa4287a3ddd4ab65651cdd01b0f0a
AureMalin <aurelien@edumalin.fr>
2023-06-27 15:56:00 +0200

    magane picture fetch for product

--- Controller/ProductController.php
+++ Controller/ProductController.php
@@ -1,5 +1,8 @@
 <?php
+
 namespace Controller;
+
+use Exception;
 
 class ProductController extends BaseController
 {
@@ -18,10 +21,28 @@ class ProductController extends BaseController
 
     public function CreateProduct($name, $description, $price, $id)
     {
-        $this->productManager->create((object)[ 
-            "name" => $name, 
-            "description" => $description, 
+        $this->productManager->create((object)[
+            "name" => $name,
+            "description" => $description,
             "price" => $price
         ]);
+    }
+
+    public function AddPicture()
+    {
+        $target_dir = "image/product/";
+        $file = pathinfo(basename($_FILES["file"]["name"]));
+
+        $target_file = $target_dir . $file["filename"] . "-" . time() . "." . $file["extension"];
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+            throw new Exception("Le format d'image n'est pas accepté");
+        } else {
+            move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
+            $response = new \stdClass();
+            $response->success = true;
+            $response->url = "/" . $target_file;
+            echo json_encode($response);
+        }
     }
 }
--- View/Product/productForm.php
+++ View/Product/productForm.php
@@ -1,6 +1,23 @@
 <form method="POST" action="/Product/New">
-    <input type="text" name="name" placeholder="name"/>
-    <input type="number" name="price" step="0.01" min="0" placeholder="price"/>
-    <textarea name="description" placeholder="description"></textarea>
-    <input type="submit" value="Créer Produit"/>
+    <h1>Créer un nouveau produit</h1>
+    <div class="form-group">
+        <input type="text" name="name" placeholder="nom du produit" />
+    </div>
+    <div class="form-group">
+        <input type="number" name="price" step="0.01" min="0" placeholder="prix" />
+    </div>
+    <div class="form-group">
+        <textarea name="description" placeholder="description"></textarea>
+    </div>
+    <div class="form-group">
+        <label class="product-file">
+            <input type="file" accept=".png,.jpg,.jpeg">
+            <span class="label">Ajouter une image (.jpg, .png)</span>
+            <img class="picture" />
+            <input type="hidden" name="picture" />
+        </label>
+    </div>
+    <div class="form-group">
+        <input type="submit" value="Créer Produit" />
+    </div>
 </form>
