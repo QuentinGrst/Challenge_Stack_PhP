5008a827e678c07939a730675c39255c2d8d2263
lucas-laza <laza.lucas15@gmail.com>
2023-06-27 11:05:02 +0200

    correction affichage des elements dans l'historique des orders

--- View/Order/orderList.php
+++ View/Order/orderList.php
@@ -1,17 +1,21 @@
-<div class="products">
+<div class="order_history">
     <?php foreach ($orders as $order) : ?>
         <?php if (!empty($order)) : ?>
         <div class="order">
             <h3>
-                <?= $order->id ?>
+                Commande du <?= (new DateTime($order->datetime))->format('d-m-Y') ?> à <?= (new DateTime($order->datetime))->format('H:m:s') ?>
             </h3>
-            <?= (new DateTime($order->datetime))->format('c') ?>
-            <?php foreach ($orderElems as $orderElem) : ?>
-                <div class="elem">
-                    <h4><?= $orderElem->product->name ?></h4>
-                    <p><?= $orderElem->product->price ?>€</p>
-                </div>
-            <?php endforeach ?>
+            <div class="elems">
+                <?php if (!empty($orderElems[$order->id])) : ?>
+                <?php foreach ($orderElems[$order->id] as $orderElem) : ?>
+                    <div class="elem">
+                        <h4><?= $orderElem->product_name ?></h4>
+                        <h5>x<?= $orderElem->quantity ?></h5>
+                        <p><?= $orderElem->product_price ?>€</p>
+                    </div>
+                <?php endforeach ?>
+                <?php endif ?>
+            </div>
         </div>
         <?php endif ?>
     <?php endforeach ?>
