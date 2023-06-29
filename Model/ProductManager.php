79d3d48434fb9a024156b442ef821117f93b4f04
lucas-laza <laza.lucas15@gmail.com>
2023-06-29 09:13:21 +0200

    Recherche: resolution erreur SQL

--- Model/ProductManager.php
+++ Model/ProductManager.php
@@ -20,7 +20,7 @@ class ProductManager extends ModelManager
 
     public function SearchProductsWithInput($search)
     {
-        $req = $this->bdd->prepare("SELECT p.*, u.pseudo as seller_name, u.id as seller_id FROM " . $this->table . " p JOIN users u ON u.id = p.seller_id WHERE (name LIKE :search OR description LIKE :search) AND state = 1");
+        $req = $this->bdd->prepare("SELECT p.*, u.pseudo as seller_name, u.id as seller_id FROM " . $this->table . " p JOIN users u ON u.id = p.seller_id WHERE (p.name LIKE :search OR p.description LIKE :search) AND state = 1");
         $searchTerm = "%" . $search . "%";
         $req->bindParam(":search", $searchTerm);
         $req->execute();
